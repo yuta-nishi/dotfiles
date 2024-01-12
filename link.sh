45d255175c5fdaa6af5a27e3d86fcdb24d7cecb3
yuta-nishi <ypmtk437.0209@gmail.com>
2024-01-13 04:50:09 +0900

    feat: add vscode settings

--- link.sh
+++ link.sh
@@ -4,3 +4,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.config/nvim/ ~/.config/
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.vscode/tasks.json /Users/yutanishi/Library/Application\ Support/Code/User/tasks.json
+ln -s ~/dotfiles/.vscode/settings.json /Users/yutanishi/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/.vscode/keybindings.json /Users/yutanishi/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/dotfiles/snippets/ /Users/yutanishi/Library/Application\ Support/Code/User/
