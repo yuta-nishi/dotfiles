94cef3fcefbee61166e8accbf4dba1dea282c492
yuta-nishi <ypmtk437.0209@gmail.com>
2024-01-13 23:27:06 +0900

    fix: correcting the path as it was incorrect

--- link.sh
+++ link.sh
@@ -7,4 +7,4 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.vscode/tasks.json /Users/yutanishi/Library/Application\ Support/Code/User/tasks.json
 ln -s ~/dotfiles/.vscode/settings.json /Users/yutanishi/Library/Application\ Support/Code/User/settings.json
 ln -s ~/dotfiles/.vscode/keybindings.json /Users/yutanishi/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/dotfiles/snippets/ /Users/yutanishi/Library/Application\ Support/Code/User/
+ln -s ~/dotfiles/.vscode/snippets/ /Users/yutanishi/Library/Application\ Support/Code/User
