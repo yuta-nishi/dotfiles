addfb29464ed0149f2b89346ebd47bb389d163a0
yuta-nishi <ypmtk437.0209@gmail.com>
2024-01-14 00:23:05 +0900

    chore: update neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,10 +1,16 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-        filesystem = {
-            filtered_items = {
-                visible = true,
-            },
-        },
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
     },
+  },
 }
