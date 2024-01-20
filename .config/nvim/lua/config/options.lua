dd6292b1f43b5efa274569c72c3befe27439a4e0
yuta-nishi <ypmtk437.0209@gmail.com>
2024-01-20 18:51:53 +0900

    chore: enable clipboard for yank

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,6 +7,7 @@ vim.opt.fileencoding = "utf-8"
 vim.opt.number = true
 vim.opt.relativenumber = false
 
+vim.opt.clipboard = "unnamedplus"
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
