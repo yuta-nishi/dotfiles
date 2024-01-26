b4d1e12f3f8c718a543729c049622a624196624e
yuta-nishi <ypmtk437.0209@gmail.com>
2024-01-26 16:10:59 +0900

    chore: add link for mac

--- link.sh
+++ link.sh
@@ -4,8 +4,8 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.config/nvim/ ~/.config/
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.vscode/tasks.json /Users/yutanishi/Library/Application\ Support/Code/User/tasks.json
-ln -s ~/dotfiles/.vscode/settings.json /Users/yutanishi/Library/Application\ Support/Code/User/settings.json
-ln -s ~/dotfiles/.vscode/keybindings.json /Users/yutanishi/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/dotfiles/.vscode/snippets/ /Users/yutanishi/Library/Application\ Support/Code/User
-ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
+# ln -s ~/dotfiles/.vscode/tasks.json /Users/yutanishi/Library/Application\ Support/Code/User/tasks.json
+# ln -s ~/dotfiles/.vscode/settings.json /Users/yutanishi/Library/Application\ Support/Code/User/settings.json
+# ln -s ~/dotfiles/.vscode/keybindings.json /Users/yutanishi/Library/Application\ Support/Code/User/keybindings.json
+# ln -s ~/dotfiles/.vscode/snippets/ /Users/yutanishi/Library/Application\ Support/Code/User
+# ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
---
+++ link_mac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.config/nvim/ ~/.config/
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
