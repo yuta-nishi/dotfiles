98313d77e93b48d1de6fd100946aababa9dfd8f0
yuta-nishi <ypmtk437.0209@gmail.com>
2024-01-30 01:32:05 +0900

    fix: update symbolic link for nvim config

--- link_mac.sh
+++ link_mac.sh
@@ -2,5 +2,5 @@
 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.config/nvim/ ~/.config/
+ln -s ~/dotfiles/.config/nvim/ ~/.config
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
