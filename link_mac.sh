8ba217df0dd6ecda0a9f1ac0f2b317aa426ccea7
yuta-nishi <ypmtk437.0209@gmail.com>
2024-04-16 02:32:33 +0900

    chore: add starship setting

--- link_mac.sh
+++ link_mac.sh
@@ -3,5 +3,6 @@
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.config/nvim/ ~/.config
+ln -s ~/dotfiles/.config/starship.toml ~/.config
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
