1a23b6f898650c5b2bcbd6e710ea15ac92999a58
yuta-nishi <ypmtk437.0209@gmail.com>
2024-07-03 18:02:00 +0900

    chore: add starship.toml

--- link_linux.sh
+++ link_linux.sh
@@ -3,5 +3,6 @@
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.config/nvim/ ~/.config
+ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
