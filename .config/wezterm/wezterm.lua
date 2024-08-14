b53213eed769160922023006239c1143ea1fd8e3
yuta-nishi <ypmtk437.0209@gmail.com>
2024-08-14 13:58:53 +0900

    chore: Change a color theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 -- color
-config.color_scheme = "dracula"
+config.color_scheme = "Everforest Dark (Gogh)"
 config.window_background_opacity = 0.88
 
 -- font
