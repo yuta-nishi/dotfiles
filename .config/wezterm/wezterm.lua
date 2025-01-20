639109afe86c1c58e12097f1c0ba6f2f246e8134
yuta-nishi <ypmtk437.0209@gmail.com>
2025-01-21 02:08:52 +0900

    chore: Update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,12 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
 
+-- general
+config.automatically_reload_config = true
+config.default_cursor_style = "BlinkingBar"
+
 -- color
-config.color_scheme = "Everforest Dark (Gogh)"
-config.window_background_opacity = 0.88
+config.color_scheme = "Catppuccin Mocha"
 
 -- font
 config.font_size = 14.0
@@ -14,10 +16,33 @@ config.line_height = 1.2
 -- scroll
 config.enable_scroll_bar = true
 
--- keybindings
-config.keys = require("keybindings").keys
+-- keys
+config.keys = require("keys").keys
 
--- screen
+-- tab
+config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 5
+
+-- window
+config.background = {
+  {
+    source = {
+      File = wezterm.home_dir .. "/.config/wezterm/evamark6.jpg",
+    },
+    hsb = {
+      brightness = 0.1,
+    },
+  },
+}
+config.initial_cols = 120
+config.initial_rows = 30
+config.macos_window_background_blur = 20
 config.native_macos_fullscreen_mode = true
+config.window_decorations = "RESIZE"
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
 
 return config
