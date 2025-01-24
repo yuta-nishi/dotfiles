b8a9d1037079d0eeafa400fcecfc9fd21c49cb88
yuta-nishi <ypmtk437.0209@gmail.com>
2025-01-24 19:26:09 +0900

    chore: Add idea options setting

--- link_mac.sh
+++ link_mac.sh
@@ -1,21 +1,48 @@
 #!/bin/zsh
 
-ln -fs "$HOME/dotfiles/.config/aerospace/" "$HOME/.config"
-ln -fs "$HOME/dotfiles/.config/mise/" "$HOME/.config"
-ln -fs "$HOME/dotfiles/.config/nix/" "$HOME/.config"
-ln -fs "$HOME/dotfiles/.config/nvim/" "$HOME/.config"
-ln -fs "$HOME/dotfiles/.config/sheldon/" "$HOME/.config"
-ln -fs "$HOME/dotfiles/.config/starship.toml" "$HOME/.config"
-ln -fs "$HOME/dotfiles/.config/wezterm/" "$HOME/.config"
-ln -fs "$HOME/dotfiles/.config/zabrze/" "$HOME/.config"
-ln -fs "$HOME/dotfiles/.config/zed/keymap.json" "$HOME/.config/zed/keymap.json"
-ln -fs "$HOME/dotfiles/.config/zed/settings.json" "$HOME/.config/zed/settings.json"
-ln -fs "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
-ln -fs "$HOME/dotfiles/.ideavimrc" "$HOME/.ideavimrc"
-ln -fs "$HOME/dotfiles/.latexmkrc" "$HOME/.latexmkrc"
-ln -fs "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
-ln -fs "$HOME/dotfiles/.vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
-ln -fs "$HOME/dotfiles/.vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-ln -fs "$HOME/dotfiles/.vscode/tasks.json" "$HOME/Library/Application Support/Code/User/tasks.json"
-ln -fs "$HOME/dotfiles/.zshenv" "$HOME/.zshenv"
-ln -fs "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+create_symlink() {
+    if ln -fs "$1" "$2"; then
+        echo "Linked: $2 -> $1"
+    else
+        echo "Failed to link: $2 -> $1"
+    fi
+}
+
+# Create symbolic links in the home folder
+echo "Creating links in the home folder..."
+create_symlink "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
+create_symlink "$HOME/dotfiles/.ideavimrc" "$HOME/.ideavimrc"
+create_symlink "$HOME/dotfiles/.latexmkrc" "$HOME/.latexmkrc"
+create_symlink "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
+create_symlink "$HOME/dotfiles/.zshenv" "$HOME/.zshenv"
+create_symlink "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+
+# Create symbolic links in the .config folder
+echo "Creating links in the .config folder..."
+create_symlink "$HOME/dotfiles/.config/aerospace/" "$HOME/.config/aerospace"
+create_symlink "$HOME/dotfiles/.config/mise/" "$HOME/.config/mise"
+create_symlink "$HOME/dotfiles/.config/nix/" "$HOME/.config/nix"
+create_symlink "$HOME/dotfiles/.config/nvim/" "$HOME/.config/nvim"
+create_symlink "$HOME/dotfiles/.config/sheldon/" "$HOME/.config/sheldon"
+create_symlink "$HOME/dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
+create_symlink "$HOME/dotfiles/.config/wezterm/" "$HOME/.config/wezterm"
+create_symlink "$HOME/dotfiles/.config/zabrze/" "$HOME/.config/zabrze"
+create_symlink "$HOME/dotfiles/.config/zed/keymap.json" "$HOME/.config/zed/keymap.json"
+create_symlink "$HOME/dotfiles/.config/zed/settings.json" "$HOME/.config/zed/settings.json"
+
+# Create symbolic links in the Visual Studio Code settings folder
+echo "Creating links in the Visual Studio Code settings folder..."
+create_symlink "$HOME/dotfiles/.vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+create_symlink "$HOME/dotfiles/.vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+create_symlink "$HOME/dotfiles/.vscode/tasks.json" "$HOME/Library/Application Support/Code/User/tasks.json"
+
+# Create symbolic links in the IntelliJ IDEA settings folder
+echo "Creating links in the IntelliJ IDEA settings folder..."
+create_symlink "$HOME/dotfiles/.idea/keymaps/default.xml" "$HOME/Library/Application Support/JetBrains/IntelliJIdea2024.3/keymaps/default.xml"
+\find "$HOME/dotfiles/.idea/options" -type f | while read -r file_path; do
+    relative_path="${file_path#$HOME/dotfiles/.idea/options/}"
+    target_path="$HOME/Library/Application Support/JetBrains/IntelliJIdea2024.3/options/$relative_path"
+    create_symlink "$file_path" "$target_path"
+done
+
+echo "All symbolic links have been created successfully."
