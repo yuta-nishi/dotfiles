3a271e08b6ef28b9e31bc1d543500aec01c8498a
yuta-nishi <ypmtk437.0209@gmail.com>
2025-02-22 16:53:15 +0900

    chore: Update link

--- link_mac.sh
+++ link_mac.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 create_symlink() {
     if ln -fs "$1" "$2"; then
@@ -10,17 +10,21 @@ create_symlink() {
 
 # Create symbolic links in the home folder
 echo "Creating links in the home folder..."
+create_symlink "$HOME/dotfiles/.condarc" "$HOME/.condarc"
 create_symlink "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
 create_symlink "$HOME/dotfiles/.ideavimrc" "$HOME/.ideavimrc"
 create_symlink "$HOME/dotfiles/.latexmkrc" "$HOME/.latexmkrc"
 create_symlink "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
+create_symlink "$HOME/dotfiles/.zprofile" "$HOME/.zprofile"
 create_symlink "$HOME/dotfiles/.zshenv" "$HOME/.zshenv"
 create_symlink "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
-create_symlink "$HOME/dotfiles/.condarc" "$HOME/.condarc"
 
 # Create symbolic links in the .config folder
 echo "Creating links in the .config folder..."
 create_symlink "$HOME/dotfiles/.config/aerospace/" "$HOME/.config"
+create_symlink "$HOME/dotfiles/.config/bat/" "$HOME/.config"
+create_symlink "$HOME/dotfiles/.config/borders/" "$HOME/.config"
+create_symlink "$HOME/dotfiles/.config/htop/" "$HOME/.config"
 create_symlink "$HOME/dotfiles/.config/mise/" "$HOME/.config"
 create_symlink "$HOME/dotfiles/.config/nix/" "$HOME/.config"
 create_symlink "$HOME/dotfiles/.config/nvim/" "$HOME/.config"
@@ -31,24 +35,24 @@ create_symlink "$HOME/dotfiles/.config/zabrze/" "$HOME/.config"
 create_symlink "$HOME/dotfiles/.config/zed/keymap.json" "$HOME/.config/zed/keymap.json"
 create_symlink "$HOME/dotfiles/.config/zed/settings.json" "$HOME/.config/zed/settings.json"
 
-# Create symbolic links in the Visual Studio Code settings folder
-echo "Creating links in the Visual Studio Code settings folder..."
+# Create symbolic links in the Application Support folder
+echo "Creating links in the Application Support folder..."
 create_symlink "$HOME/dotfiles/.vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
 create_symlink "$HOME/dotfiles/.vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 create_symlink "$HOME/dotfiles/.vscode/tasks.json" "$HOME/Library/Application Support/Code/User/tasks.json"
 \find "$HOME/dotfiles/.vscode/snippets" -type f | while read -r file_path; do
-    relative_path="${file_path#$HOME/dotfiles/.vscode/snippets/}"
+    relative_path="${file_path#"$HOME/dotfiles/.vscode/snippets/"}"
     target_path="$HOME/Library/Application Support/Code/User/snippets/$relative_path"
     create_symlink "$file_path" "$target_path"
 done
 
-# Create symbolic links in the IntelliJ IDEA settings folder
-echo "Creating links in the IntelliJ IDEA settings folder..."
 create_symlink "$HOME/dotfiles/idea/keymaps/default.xml" "$HOME/Library/Application Support/JetBrains/IntelliJIdea2024.3/keymaps/default.xml"
 \find "$HOME/dotfiles/idea/options" -type f | while read -r file_path; do
-    relative_path="${file_path#$HOME/dotfiles/idea/options/}"
+    relative_path="${file_path#"$HOME/dotfiles/idea/options/"}"
     target_path="$HOME/Library/Application Support/JetBrains/IntelliJIdea2024.3/options/$relative_path"
     create_symlink "$file_path" "$target_path"
 done
+
+create_symlink "$HOME/dotfiles/lazygit/config.yml" "$HOME/Library/Application Support/lazygit/config.yml"
 
 echo "All symbolic links have been created successfully."
