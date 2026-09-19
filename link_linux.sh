afd1a3c37054661d97ee201aa17391f056755633
yuta-nishi <ypmtk437.0209@gmail.com>
2026-09-19 14:53:25 +0900

    chore: drop unused Linux support files

--- link_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ln -f -s "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
-ln -f -s "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
-ln -f -s "$HOME/dotfiles/.config/nvim/" "$HOME/.config/nvim"
-ln -f -s "$HOME/dotfiles/.config/starship.toml" "$HOME/.config/starship.toml"
-ln -f -s "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
-ln -f -s "$HOME/dotfiles/.latexmkrc" "$HOME/.latexmkrc"
